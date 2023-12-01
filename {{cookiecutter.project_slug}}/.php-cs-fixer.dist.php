87c9aeea335eeaf816f3380d105d169511e9fe4a
Jérôme Gamez <jerome@gamez.name>
2023-12-01 05:35:12 +0100

    Update code style

--- {{cookiecutter.project_slug}}/.php-cs-fixer.dist.php
+++ {{cookiecutter.project_slug}}/.php-cs-fixer.dist.php
@@ -8,5 +8,14 @@ return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@PER-CS2.0' => true,
+        'no_unused_imports' => true,
+        'ordered_imports' => [
+            'imports_order' => [
+                'class',
+                'function',
+                'const',
+            ],
+            'sort_algorithm' => 'alpha',
+        ],
     ])
     ->setFinder($finder);
