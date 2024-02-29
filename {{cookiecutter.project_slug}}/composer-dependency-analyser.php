c17ff72089aba597c64b5568c8af7f584a8acec5
Jérôme Gamez <jerome@gamez.name>
2024-02-29 01:46:41 +0100

    Add `shipmonk/composer-dependency-analyser`

---
+++ {{cookiecutter.project_slug}}/composer-dependency-analyser.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration())
+    ->addPathToScan(__FILE__, true)
+    ->addPathToScan(__DIR__.'/src', false)
+    ->addPathToScan(__DIR__.'/tests', true)
+;
