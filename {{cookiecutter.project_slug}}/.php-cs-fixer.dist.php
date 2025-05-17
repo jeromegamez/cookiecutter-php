54b83122e0f1060a81ae1c960e3ab1127d44aac1
Jérôme Gamez <jerome@gamez.name>
2025-05-18 01:55:42 +0200

    Replace `beste/php-cs-fixer-config` with minimal PHP CS Fixer config

--- {{cookiecutter.project_slug}}/.php-cs-fixer.dist.php
+++ {{cookiecutter.project_slug}}/.php-cs-fixer.dist.php
@@ -2,11 +2,22 @@
 
 declare(strict_types=1);
 
-use Beste\PhpCsFixer\Config\Factory;
-use Beste\PhpCsFixer\Config\RuleSet\Php83;
-
 $finder = PhpCsFixer\Finder::create()->in(__DIR__);
 
-return (Factory::fromRuleSet(new Php83()))
+return (new PhpCsFixer\Config())
+    ->setParallelConfig(
+        PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect()
+    )
     ->setRiskyAllowed(true)
+    ->setRules([
+        '@PER-CS2.0' => true,
+        'multiline_whitespace_before_semicolons' => [
+            'strategy' => 'new_line_for_chained_calls',
+        ],
+        'no_unused_imports' => true,
+        'php_unit_internal_class' => [
+            'types' => ['abstract', 'final', 'normal'],
+        ],
+        'trim_array_spaces' => true,
+    ])
     ->setFinder($finder);
